b8678a147cc36e09c8c6a75bcfc8a7cb63848dbe
NowickiBlazej <blazej.a.now@gmail.com>
2022-04-19 20:48:05 +0200

    details/list view, propositions, voting

--- event/views.py
+++ event/views.py
@@ -1,12 +1,16 @@
+from datetime import datetime
+from django.forms import DateTimeField
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.utils.translation import gettext as _
 from django.shortcuts import get_object_or_404
+from django.db.models import Count
+from django.utils.timezone import now
 
 from catalog.models import Follower
 
 from .forms import EventForm
-from .models import Event
+from .models import Event, Proposition, PropositionVote
 
 
 def event_list(request):
@@ -20,8 +24,42 @@ def event_list(request):
 
 
 def event_detail(request, pk):
+    if not request.user.is_authenticated:
+        redirect("/user/")
+
     event = get_object_or_404(Event, pk=pk)
-    return render(request, 'event/event_detail.html', {'event': event})
+
+    if request.method == "POST":
+        if 'vote' in dict(request.POST):
+            proposition_id = int(request.POST['vote'])
+            proposition_vote = PropositionVote.objects.filter(author=request.user, proposition_id=proposition_id)
+            if not proposition_vote.exists():
+                PropositionVote(proposition=list(Proposition.objects.filter(id=proposition_id))[0],
+                                author=request.user,
+                                datetime=now).save()
+        
+        if 'date' in dict(request.POST):
+            # print(request.POST['date'])
+            dt = datetime.strptime(request.POST['date'], '%Y-%m-%dT%H:%M')
+            Proposition(author=request.user,
+                        event=event,
+                        datetime=dt).save()
+
+    propositions = list(Proposition.objects.filter(event=event))
+    votes = PropositionVote.objects.values('proposition').annotate(total=Count('proposition')).order_by('proposition')
+
+    votes_as_dict = {}
+    for vote in votes:
+        votes_as_dict[vote['proposition']] = vote['total']
+    
+    for prop in propositions:
+        if prop.id in votes_as_dict.keys():
+            prop.votes = votes_as_dict[prop.id]
+            prop.voted = PropositionVote.objects.filter(author=request.user, proposition_id=prop.id).exists()
+        else:
+            prop.votes = 0
+            prop.voted = False
+    return render(request, 'event/event_detail.html', {'event': event, 'propositions': propositions})
 
 
 def event_create(request):
@@ -32,5 +70,4 @@ def event_create(request):
             return redirect('event:list')
     else:
         form = EventForm()
-
     return render(request, 'event/event_create.html', {'form': form})
